fb380d38288f34038f11e0f7efa17ae42da3932a
Dinesh Vaitage <dinesh@coderkeys.com>
2024-06-26 00:01:48 +0530

    comment white listed for now

--- app/Http/Middleware/WhitelistHost.php
+++ app/Http/Middleware/WhitelistHost.php
@@ -31,10 +31,10 @@ class WhitelistHost
 
         // Check if the host is in the whitelist
          // Check if the User-Agent contains "PostmanRuntime"
-        if (!in_array($host, $whitelistedHosts) && strpos($userAgent, 'PostmanRuntime') === false) {
+       /*  if (!in_array($host, $whitelistedHosts) && strpos($userAgent, 'PostmanRuntime') === false) {
             // If not, return a 403 Forbidden response
             return response('Forbidden', 403);
-        }
+        } */
 
         // If the host is whitelisted, proceed with the request
         return $next($request);
---
+++ database/migrations/2024_06_24_181818_update_appointments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('details');
+            $table->date('date');
+            $table->time('time');
+            $table->enum('status', ['ACTIVE', 'INACTIVE'])->default('ACTIVE');
+            $table->unsignedBigInteger('patient_id');
+            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('doctor_id')->references('id')->on('employees')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
