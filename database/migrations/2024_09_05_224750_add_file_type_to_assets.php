4d14207b173ffaea69e4d3f64b7672d9d8e02c9d
Dinesh Vaitage <dinesh@accrualify.com>
2024-09-05 22:17:30 +0530

    migration file attachment

--- database/migrations/2024_09_05_224750_add_file_type_to_assets.php
+++ database/migrations/2024_09_05_224750_add_file_type_to_assets.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddClinicIdToAssets extends Migration
+class AddFileTypeToAssets extends Migration
 {
     /**
      * Run the migrations.
