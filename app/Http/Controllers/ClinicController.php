060bb20b4c8272b093bc309d691bc1c83f803ae1
Dinesh Vaitage <dinesh@accrualify.com>
2024-09-07 21:58:57 +0530

    logo upload

--- app/Http/Controllers/ClinicController.php
+++ app/Http/Controllers/ClinicController.php
@@ -109,9 +109,9 @@ class ClinicController extends Controller
 
     public function upload_logo(Request $request, $id)
     {
-        $request->validate([
+    /*     $request->validate([
             'logo' => 'required|file|mimes:jpg,png,jpeg|max:2048',
-        ]);
+        ]); */
 
          // Find the 
          $clinic = Auth::user()->clinic;
