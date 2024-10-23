9942c8ff310118d0e338183216656eafc8905105
Dinesh Vaitage <dinesh@coderkeys.com>
2024-10-23 22:53:29 +0530

    changes to fix issue

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -65,7 +65,7 @@ class InvoiceController extends Controller
 
         // Create the invoice
         $invoice = Invoice::create($request->only( ['amount', 'payment_date','paid_by','transaction_number','description','status','clinic_id','patient_id']));
-        $rand = str()->random(16);
+        $rand = Str::random(16);
         $invoice->rnd_number = $invoice->id.".".$clinic_id.".".$rand;
         $invoice->save();
 
@@ -83,7 +83,7 @@ class InvoiceController extends Controller
         $invoice = Auth::user()->clinic->invoices->find($id);
 
         if(!$invoice->rnd_number){
-            $rand = str()->random(16);
+            $rand = Str::random(16);
             $invoice->rnd_number = $invoice->id.".".$clinic_id.".".$rand;
             $invoice->save();
         }
